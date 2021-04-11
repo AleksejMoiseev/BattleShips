26c74e03d411b5aa765f4e7f921d30f9e7b60d42
Moiseev Aleksej <moiseev.003@bk.ru>
2021-04-11 19:38:28 +0300

    Add projects battleships

---
+++ SeaWar/battle_of_ships/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BattleOfShipsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'battle_of_ships'
---
+++ SeaWar/battle_of_ships/views.py
@@ -0,0 +1,164 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from battle_of_ships.forms import CreateUserForm
+from battle_of_ships.serializers import *
+from battle_of_ships.models import User as u
+from battle_of_ships.models import Game
+from rest_framework.permissions import AllowAny
+from django.views.generic import CreateView
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User as auth
+
+
+from rest_framework import authentication
+from rest_framework import exceptions
+
+from django.conf import settings
+
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.utils.decorators import method_decorator
+
+
+
+class ExampleAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        username = request.META.get('HTTP_X_USERNAME')
+        if not username:
+            return None
+
+        try:
+            user = auth.objects.get(username=username)
+            print(user)
+        except auth.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No such user')
+
+        return (user, None)
+
+# Create your views here.
+
+
+def myfunc(request):
+    return HttpResponse("<h1>Privet</h1>")
+
+
+class GameCreateView(generics.ListCreateAPIView):
+    serializer_class = GameDetailSerializers
+    queryset = Game.objects.all()
+
+
+@method_decorator(csrf_exempt, name='create')
+class UserCreateView(generics.ListCreateAPIView, ExampleAuthentication):
+    serializer_class = UserListSerializers
+    queryset = u.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        self.authenticate(request)
+        # request.set_cookie(key='id', value=38)
+        print(request.user)
+        print(request.session)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        id_user = serializer.data['id']
+        # request.session['id'] = id_user
+        # print(request.session)
+        # print(request.session['id'])
+        headers['id'] = id_user
+        response = Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        response.set_cookie(key='id', value=id_user)
+        return response
+
+
+class GameRetriveView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = GameDetailSerializers
+    queryset = Game.objects.all()
+    lookup_field = 'id'
+    permission_classes = (AllowAny, )
+
+"""
+Еще одна реализация через написания логики в самом классе через наследования базового класса APIView,  и 
+самостоятельному определению метода get
+"""
+
+
+class GameDetailView(APIView):
+
+    def get(self, request, id):
+        game = Game.objects.get(id=id)  # выбираем из базы данных   query данные с id=id
+        serializer = GameDetailSerializers(game)  # устанавливаем сериализатор
+        return Response(serializer.data)  # Возвращаем Responce c данными
+
+
+class UserDestroy(generics.RetrieveDestroyAPIView):
+    serializer_class = UserListSerializers
+    queryset = u.objects.all()
+
+
+def assign_const(request):
+    settings.MY_CONST = 2222
+    print(settings.MY_CONST)
+    return HttpResponse(settings.MY_CONST)
+
+def view_const(request):
+    from django.conf import settings
+    print(settings.MY_CONST)
+    return HttpResponse(settings.MY_CONST)
+
+
+# class CreateUserAndGame(CreateView):
+#     form_class = CreateUserForm
+#     template_name = "my_server/fronted/auth.html"
+#     context_object_name = 'name_user'
+#
+#
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CreateUserAndGame(generics.CreateAPIView):
+    serializer_class = CreateStartGame
+    queryset = Game.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        name = request.data['name']
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        id_game = serializer.data['id']
+        game = Game.objects.get(id=id_game)
+        created_user = u.objects.create(name=name, game=game)
+        headers = self.get_success_headers(serializer.data)
+        headers['id_user'] = created_user.pk
+        response = Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        response.set_cookie(key='id', value=created_user.pk)
+        return response
+
+
+class UpdateShip(generics.RetrieveUpdateAPIView):
+    serializer_class = UserShipStatusUpdate
+    queryset = u.objects.all()
+
+
+@csrf_exempt
+def faire(request):
+    print(request.POST)
+    response = HttpResponse()
+    response.set_cookie(key='set_cookie', value='B3')
+    print("proba")
+    response.content = b'B3'
+    return response
+
+
+
+
+
+
+
+
+
+
