b0708d3bd3afa7624ff1aab5a27d37dfcdcd3e95
Moiseev Aleksej <moiseev.003@bk.ru>
2021-05-28 13:00:31 +0300

    delete print

--- SeaWar/battle_of_ships/views.py
+++ SeaWar/battle_of_ships/views.py
@@ -56,12 +56,8 @@ class UserCreateView(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         # self.authenticate(request)
-        print(request.user)
-        print(request.session)
-        print("request data", request.data)
         data_request = request.data
         game_id = data_request['game']
-        print(game_id)
         users = User.objects.select_for_update().filter(game_id=game_id)
         with transaction.atomic():
             if len(users) >= MAXIMUM_ALLOWED_NUMBER_OF_PLAYERS:
@@ -114,7 +110,6 @@ class CreateUserAndGame(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         id_game = serializer.data['id']
-        print('id_Game', id_game)
         game = Game.objects.get(id=id_game)
         created_user = User.objects.create(name=name, game=game)
         headers = self.get_success_headers(serializer.data)
@@ -129,8 +124,6 @@ class UpdateShip(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
 
     def update(self, request, *args, **kwargs):
-        print('request', request.COOKIES)
-        print("request cookies", request.COOKIES.get('id'))
         user_id = request.COOKIES.get('id')
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
@@ -143,8 +136,7 @@ class UpdateShip(generics.RetrieveUpdateAPIView):
             # forcibly invalidate the prefetch cache on the instance.
             instance._prefetched_objects_cache = {}
 
-        print(choice_ready_user(user_id=user_id))
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
 
 
 @csrf_exempt
@@ -155,38 +147,28 @@ def faire(request):
 
     game, list_of_ships_enemy = get_enemy_ships(user_id=user_id)
     harbor_arr_ships_enemy = get_array_ships(list_json_objects=list_of_ships_enemy)
-    print(harbor_arr_ships_enemy)
     hit_result = check_hit(harbor=harbor_arr_ships_enemy, coordinate=coordinate)
-    print('hit_result', hit_result)
     harbor_arr_ready_save_to_database = ships_to_json(arrships_objects=harbor_arr_ships_enemy)
     save_enemy_ship(ships_json=harbor_arr_ready_save_to_database, user_id=user_id)
-    print("check_winner(arr_ships=harbor_arr_ships_enemy)", check_winner(arr_ships=harbor_arr_ships_enemy))
     if check_winner(arr_ships=harbor_arr_ships_enemy):
         user = User.objects.get(pk=user_id)
         user_name = user.name
         user.game.status = Game.Status.FINISHED
         user.game.winner = user_name
         user.game.save()
-        print("СТАТУС ИГРЫ - Зашли в победу", game.status)
         data = {"coordinate": coordinate, "hit_result": "win", "winner": user_name}
         content = JSONRenderer().render(data=data)
-        print("content", content)
         response.content = content
         return response
     if hit_result == 'mimo':
         next_move_user, game = next_move(user_id=user_id)
-        print('следующий ход делает user:', next_move_user)
-        redis_key = 'current_' + str(game.id)
-        print('value redis', redis.get(name=redis_key))
 
     redis_key_set_shots = SET_CONTAINS_SHOTS_USER_KEY_PREFIX + str(game.id) + str(user_id)
-    # print("redis_key_set_shots", redis_key_set_shots)
 
     redis.sadd(redis_key_set_shots, coordinate)
     data = {"coordinate": coordinate, "hit_result": hit_result}
     content = JSONRenderer().render(data=data)
 
-    print(content)
     response.content = content
     return response
 
@@ -201,22 +183,18 @@ def get_current_move(request):
     winner = user.game.winner
     response = HttpResponse()
     if status == 0:
-        print('Мы здесь')
         data = {"current_move": "null", "status": status, "winner": winner}
         content = JSONRenderer().render(data=data)
         response.content = content
         return response
 
     redis_key = 'current_' + str(game.id)
-    print(redis_key)
     current_move = redis.get(name=redis_key)
 
     data = {"current_move": current_move, "status": status, "winner": winner}
     content = JSONRenderer().render(data=data)
     response.content = content
 
-    # response.content = current_move
-    print("current_move", current_move)
     return response
 
 @csrf_exempt
@@ -224,20 +202,14 @@ def get_current_move(request):
 def get_shots_enemy(request):
     try:
         user_id = request.COOKIES.get('id')
-        print('user_id request', user_id)
         if user_id == None:
             raise DoesNotUser
     except DoesNotUser:
-        print(" i here")
         return HttpResponse("<h1>Нет USERA</h1>")
     game, user_enemy = get_enemy(user_id=user_id)
     redis_key_get_shots = SET_CONTAINS_SHOTS_USER_KEY_PREFIX + str(game.id) + str(user_enemy.id)
-    print(" redis_key_get_shots", redis_key_get_shots)
     set_shots_enemy = redis.smembers(redis_key_get_shots)
-    # set_shots_enemy = redis.smembers(name='myset2')
     set_shots_enemy_in_json = JSONRenderer().render(data=set_shots_enemy)
-    print("set_shots_enemy_in_json", set_shots_enemy_in_json)
-    print("set_shots_enemy", set_shots_enemy)
     return HttpResponse(content=set_shots_enemy_in_json)
 
 
